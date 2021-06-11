7e89043420567c00f17fc1924a2ef41b9f46d9ca
Mitchell Gritts <mitchellgritts@pm.me>
2021-06-11 15:11:57 -0700

    🎉 init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+  </div>
+  <div>
+    <h2 class="bg-red-500">HELLO WORLD</h2>
+    <p class="text-olive">more text Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates magni nisi esse. Beatae impedit recusandae vel dignissimos. Molestiae enim qui voluptas eveniet, eos eaque officiis doloremque amet fugit veniam.</p>
   </div>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+    './src/**/*.{vue,js}'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        carolina: '#589fd6',
+        ginger: '#e0dd89',
+        almond: '#ffead1',
+        oxford: {
+          lightest: '#2e598a',
+          lighter: '#24456b',
+          DEFAULT: '#19304b',
+          darker: '#14273d',
+          darkest: '#0f1e2e'
+        },
+        olive: {
+          lightest: '#acc3af',
+          lighter: '#95b298',
+          DEFAULT: '#7a9e7f',
+          darker: '#678e6c',
+          darkest: '#56765a'
+        },
+        saffron: {
+          lightest: '#f8c9a0',
+          lighter: '#f6b379',
+          DEFAULT: '#f29647',
+          darker: '#f0882d',
+          darkest: '#e57310'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
