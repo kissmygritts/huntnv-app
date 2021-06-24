84d14ecccc76cda585609df9bad4eeb724a0a2bd
iholl <ian.michael.holl@gmail.com>
2021-06-24 16:23:35 -0700

    Added: basic styling for cards in hunts view

--- src/views/hunts/index.vue
+++ src/views/hunts/index.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="w-full overflow-y-auto">
-    <h1 class="font-bold text-xl">Hunts</h1>
-    <p v-if="loading">LOADING...</p>
-    <div class="">
-      <div v-for="hunt in hunts" :key="hunt.id" :hunt="hunt" class="">
-        <router-link :to="{ name: 'HuntDetails', params: { id: hunt.id }}">
-          <div class="flex m-10">
-            <h2 class="font-medium capitalize">
-              {{ hunt.display_name }}
-            </h2>
-            <div class="text-gray-700 font-light capitalize">
-              <span>{{ hunt.draw_type }}</span>
-              <span class="mx-1">&ndash;</span>
-              <span>{{ hunt.weapon }}</span>
+  <div class="bg-gray-200 w-full overflow-y-auto">
+    <div class="p-10">
+      <h1 class="font-bold text-2xl">Hunts</h1>
+      <div>
+        <p v-if="loading">LOADING...</p>
+        <div v-for="hunt in hunts" :key="hunt.id" :hunt="hunt">
+          <router-link :to="{ name: 'HuntDetails', params: { id: hunt.id }}">
+            <div class="bg-oxford-700 hover:bg-oxford-500 my-10 p-10 text-white rounded-md">
+              <h2 class="font-bold capitalize">
+                {{ hunt.display_name }}
+              </h2>
+              <div class=" font-light capitalize">
+                <span>{{ hunt.draw_type }}</span>
+                <span class="mx-1">&ndash;</span>
+                <span>{{ hunt.weapon }}</span>
+              </div>
+              <div class="flex font-thin">
+                <div class="pr-5 capitalize">Season Dates: {{ hunt.season_dates }}</div>
+                <div class="pr-5 capitalize">Hunt Units: {{ hunt.unit_group }}</div>
+                <div>Public Land: 50%</div>
+              </div>
             </div>
-            <div class="capitalize">Season Dates: {{ hunt.season_dates }}</div>
-            <div class="capitalize">Hunt Units: {{ hunt.unit_group }}</div>
-          </div>
-        </router-link>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -37,7 +42,7 @@ export default {
   async created () {
     this.loading = true
     const { data } = await getHunts()
-    this.hunts = data
+    this.hunts = data.filter(data => (data.species === 'elk' && data.weapon === 'muzzleloader'))
     this.loading = false
   }
 }
