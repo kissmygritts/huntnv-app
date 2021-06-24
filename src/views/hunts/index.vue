b38dd212b47eba97139dde96634adfedcf4a4da1
iholl <ian.michael.holl@gmail.com>
2021-06-24 13:59:36 -0700

    Added: basic styling for hunts view

--- src/views/hunts/index.vue
+++ src/views/hunts/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="">
-    <h1>Hunts</h1>
-    {{ hunts }}
+  <div class="w-full overflow-y-auto">
+    <h1 class="font-bold text-xl">Hunts</h1>
+    <p v-if="loading">LOADING...</p>
+    <div class="">
+      <div v-for="hunt in hunts" :key="hunt.id" :hunt="hunt" class="">
+        <router-link :to="{ name: 'HuntDetails', params: { id: hunt.id }}">
+          <div class="flex m-10">
+            <h2 class="font-medium capitalize">
+              {{ hunt.display_name }}
+            </h2>
+            <div class="text-gray-700 font-light capitalize">
+              <span>{{ hunt.draw_type }}</span>
+              <span class="mx-1">&ndash;</span>
+              <span>{{ hunt.weapon }}</span>
+            </div>
+            <div class="capitalize">Season Dates: {{ hunt.season_dates }}</div>
+            <div class="capitalize">Hunt Units: {{ hunt.unit_group }}</div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
